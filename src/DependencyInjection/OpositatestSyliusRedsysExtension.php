cf1e91e536764f47a9f2b96f55c07bb8814fa6c6
jota <jdeveloper.inxenio@gmail.com>
2018-01-07 18:11:31 +0100

    Added skeleton files

---
+++ src/DependencyInjection/OpositatestSyliusRedsysExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Opositatest\OpositatestSyliusRedsysPlugin\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+final class OppositatestSyliusRedsysExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+    }
+}
---
+++ src/OpositatestSyliusRedsysPlugin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Opositatest\OpositatestSyliusRedsysPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class Opositatest\OpositatestSyliusRedsysPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
